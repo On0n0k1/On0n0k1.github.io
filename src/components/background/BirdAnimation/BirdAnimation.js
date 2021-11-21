1c919d6137bace15e80229ee964edea8202ca843
On0n0k1 <stiltztinkerstein@gmail.com>
2021-11-21 10:05:49 -0300

    Camera moves with input now. But need to make camera rotation more smooth next. It just sticks to the next target in the first frame.

--- src/components/background/BirdAnimation/BirdAnimation.js
+++ src/components/background/BirdAnimation/BirdAnimation.js
@@ -1,5 +1,6 @@
 import { loadBirds } from './components/birds/birds.js';
-import { createCamera } from './components/camera.js';
+// import { createCamera } from './components/camera.js';
+import BirdFocusCamera from './components/camera.js';
 import { createLights } from './components/lights.js';
 import { createScene } from './components/scene.js';
 
@@ -34,7 +35,9 @@ const invisible_layer = getCSS('--z-index-background-invisible');
 class BirdAnimation {
   // constructor(container) {
 constructor() {
-    camera = createCamera();
+    this.birdFocusCamera = new BirdFocusCamera();
+    // camera = createCamera();
+    camera = this.birdFocusCamera.getCamera();
     renderer = createRenderer('#bg-birds');
     scene = createScene();
     loop = new Loop(camera, scene, renderer);
@@ -43,7 +46,8 @@ constructor() {
 
     const { ambientLight, mainLight } = createLights();
 
-    loop.updatables.push(controls);
+    // loop.updatables.push(controls);
+    loop.updatables.push(controls, this.birdFocusCamera);
     scene.add(ambientLight, mainLight);
 
     // const resizer = new Resizer(container, camera, renderer);
@@ -78,19 +82,50 @@ constructor() {
     loop.stop();
   }
 
+
+  // this.currentPosition = [-1.5, 1.5, 6.5];
+  // parrot.position.set(0, 0, 2.5);
+  // flamingo.position.set(7.5, 0, -10);
+  // stork.position.set(0, -2.5, -10);
+
+  // Birds positions (starts at parrot)
+  // stork: [0, -2.5, -10]
+  // parrot: [0, 0, 2.5]
+  // flamingo: [7.5, 0, -10]
+  // camera starts in position [-1.5, 1.5, 6.5]
+  // so camera distance vector to parrot is [ -1.5 - 0, 1.5 -0, 6.5 - 2.5] = [-1.5, 1.5, 4.0]
+  // for stork camera will then be [0 - 1.5, -2.5 + 1.5, -10 + 4.0] = [-1.5, -1.0, -6.0]
+  // for flamingo camera will then be [7.5 - 1.5, 0 + 1.5, -10 + 4.0] = [6.0, 1.5, -6.0]
+  //
+  // for ease of read, again:
+  // stork: [-1.5, 1.0, -6.0]
+  // parrot: [-1.5, 1.5, 6.5]
+  // flamingo: [6.0, 1.5, -6.0]
+
   focusStork(){
     this.birdFocus = 1;
-    console.log("Focus Stork enabled.");
+    // console.log("Focus Stork enabled.");
+    // let value = [-1.5, 1.0, -6.0];
+    // this.birdFocusCamera.moveTo(value);
+    this.birdFocusCamera.focusStork();
+
+
   }
 
   focusParrot(){
     this.birdFocus = 2;
-    console.log("Focus Parrot enabled.");
+    // console.log("Focus Parrot enabled.");
+    // let value = [-1.5, 1.5, 6.5];
+    // this.birdFocusCamera.moveTo(value);
+    this.birdFocusCamera.focusParrot();
   }
 
   focusFlamingo(){
     this.birdFocus = 3;
-    console.log("Focus Flamingo enabled")
+    // console.log("Focus Flamingo enabled");
+    // let value = [6.0, 1.5, -6.0];
+    // this.birdFocusCamera.moveTo(value);
+    this.birdFocusCamera.focusFlamingo();
   }
 
   getFocus() {
